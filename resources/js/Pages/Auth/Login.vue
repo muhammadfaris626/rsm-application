7c5850c5d1993259e06c204702f388353e5a61cb
muhammadfaris626 <muhammadfaris5795@gmail.com>
2025-12-10 22:07:06 +0800

    asd

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -48,7 +48,7 @@ const togglePasswordVisibility = () => {
                 <div class="mb-4 transform transition-transform duration-300 hover:scale-105">
                     <img :src="'images/rsm-merah.png'" class="h-24 mx-auto drop-shadow-lg">
                 </div>
-                <h1 class="text-2xl font-bold text-white mb-2">Selamat Datang</h1>
+                <h1 class="text-2xl font-bold text-white mb-2">Selamat Datang11</h1>
                 <p class="text-red-100 text-sm">Silakan masuk ke akun Anda</p>
             </div>
 
@@ -76,9 +76,9 @@ const togglePasswordVisibility = () => {
                                     <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input 
-                                v-model="form.login" 
-                                type="text" 
+                            <input
+                                v-model="form.login"
+                                type="text"
                                 placeholder="Masukkan username atau email"
                                 autocomplete="username"
                                 class="w-full pl-12 pr-4 py-3.5 border-2 border-gray-300 rounded-xl focus:ring-2 focus:ring-red-500 focus:border-red-500 transition-all duration-200 text-gray-700 placeholder-gray-400 bg-gray-50 focus:bg-white"
@@ -98,9 +98,9 @@ const togglePasswordVisibility = () => {
                                     <path fill-rule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input 
+                            <input
                                 :type="showPassword ? 'text' : 'password'"
-                                v-model="form.password" 
+                                v-model="form.password"
                                 placeholder="Masukkan kata sandi"
                                 autocomplete="current-password"
                                 class="w-full pl-12 pr-12 py-3.5 border-2 border-gray-300 rounded-xl focus:ring-2 focus:ring-red-500 focus:border-red-500 transition-all duration-200 text-gray-700 placeholder-gray-400 bg-gray-50 focus:bg-white"
@@ -127,16 +127,16 @@ const togglePasswordVisibility = () => {
                     <!-- Remember Me & Forgot Password -->
                     <div class="flex items-center justify-between">
                         <label class="flex items-center cursor-pointer group">
-                            <input 
-                                v-model="form.remember" 
-                                type="checkbox" 
+                            <input
+                                v-model="form.remember"
+                                type="checkbox"
                                 class="w-4 h-4 text-red-600 border-gray-300 rounded focus:ring-red-500 cursor-pointer"
                             >
                             <span class="ml-2 text-sm text-gray-600 group-hover:text-gray-800">Ingat saya</span>
                         </label>
-                        <Link 
+                        <Link
                             v-if="canResetPassword"
-                            :href="route('password.request')" 
+                            :href="route('password.request')"
                             class="text-sm font-medium text-red-600 hover:text-red-700 transition-colors"
                         >
                             Lupa kata sandi?
@@ -144,10 +144,10 @@ const togglePasswordVisibility = () => {
                     </div>
 
                     <!-- Submit Button -->
-                    <button 
-                        type="submit" 
+                    <button
+                        type="submit"
                         class="w-full py-3.5 px-4 bg-gradient-to-r from-red-600 to-red-700 hover:from-red-700 hover:to-red-800 text-white font-bold rounded-xl shadow-lg hover:shadow-xl transform transition-all duration-200 hover:scale-[1.02] active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed disabled:transform-none"
-                        :class="{ 'opacity-50 cursor-not-allowed': form.processing }" 
+                        :class="{ 'opacity-50 cursor-not-allowed': form.processing }"
                         :disabled="form.processing"
                     >
                         <span v-if="!form.processing" class="flex items-center justify-center">
